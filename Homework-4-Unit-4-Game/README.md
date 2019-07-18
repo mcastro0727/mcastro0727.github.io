3bef2e98d846f31717ca8fe18c140a272265cabb
mcastro0727 <51147249+mcastro0727@users.noreply.github.com>
2019-07-18 00:00:04 -0400

    Add files via upload

---
+++ Homework-4-Unit-4-Game/README.md
@@ -0,0 +1,1 @@
+# Homework-4-Unit-4-Game
---
+++ Homework-4-Unit-4-Game/assets/javascript/game.js
@@ -0,0 +1,29 @@
+// 
+    // Create number to guess
+    // Assign number attributes to crystals (1-15) dynamic
+    // Addition on click
+    // Alert win or lose
+    // 
+    // Crystal pictures
+    // 
+
+    // Global variables
+    var wins = 0
+    var losses = 0
+    var userScore = 0
+
+
+    // To select random number
+    var randomNumber = Math.floor(Math.random() * 120) + 19
+
+    console.log(randomNumber)
+
+    // To assign values to each crystal
+    var rubyValue = Math.floor(Math.random() * 12) + 1
+    var diamondValue = Math.floor(Math.random() * 12) + 1
+    var sapphireValue = Math.floor(Math.random() * 12) + 1
+    var emeraldValue = Math.floor(Math.random() * 12) + 1
+
+    console.log(rubyValue)
+
+    // 
